9eab7154e1ca8f37dc09c08058e9e76fc189c786
Gregory Arcara <gregoryarcara@Gregorys-MacBook-Pro.local>
2015-11-10 15:06:53 -0800

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,42 @@
+var should = require('should'),
+  rewire = require('rewire'),
+  convert = rewire('../convert');
+
+describe('helionce-node/homepage', function (done) {
+  var context = {};
+
+  before(function (done) {
+    done();
+  }),
+
+  it('should round to the nearest tenth', function () {
+    var roundToNearestTenth = convert.__get__('roundToNearestTenth');
+    roundToNearestTenth(1.22).should.be.equal(1.2);
+    roundToNearestTenth(-1.22).should.be.equal(-1.2);
+    roundToNearestTenth(11.56).should.be.equal(11.6);
+    roundToNearestTenth(-11.51).should.be.equal(-11.5);
+  })
+
+  it('should convert celcius to fahrenheit', function () {
+    var result = convert.celciusToFahrenheit(10);
+    result.should.be.equal(50);
+    result = convert.celciusToFahrenheit(50);
+    result.should.be.equal(122);
+    result = convert.celciusToFahrenheit(101);
+    result.should.be.equal(213.8);
+    result = convert.celciusToFahrenheit(-29);
+    result.should.be.equal(-20.2);
+  });
+
+  it('should convert fahrenheit to celcius', function () {
+    var result = convert.fahrenheitToCelcius(80);
+    result.should.be.equal(26.7);
+    result = convert.fahrenheitToCelcius(16);
+    result.should.be.equal(-8.9);
+    result = convert.fahrenheitToCelcius(0);
+    result.should.be.equal(-17.8);
+    result = convert.fahrenheitToCelcius(-19);
+    result.should.be.equal(-28.3);
+  })
+
+});
